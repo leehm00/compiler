f5df812777107ccbc47e967bd8c7b31b6c18733d
leehm00 <leehm00@qq.com>
2021-01-04 22:39:38 +0800

    常量传播

--- src/optimization/ActiveVars.cpp
+++ src/optimization/ActiveVars.cpp
@@ -6,22 +6,46 @@ void ActiveVars::run()
     output_active_vars.open("active_vars.json", std::ios::out);
     output_active_vars << "[";
     for (auto &func : this->m_->get_functions()) {
-        if (func->get_basic_blocks().empty()) {
-            continue;
-        }
-        else
-        {
             func_ = func;  
 
             func_->set_instr_name();
             live_in.clear();
             live_out.clear();
             
-            // 在此分析 func_ 的每个bb块的活跃变量，并存储在 live_in live_out 结构内
-
+            //在此分析 func_ 的每个bb块的活跃变量，并存储在 live_in live_out 结构内
+            std::map<BasicBlock *, std::set<Value *>> use;
+            std::map<BasicBlock *, std::set<Value *>> def;
+            for (auto bb : func->get_basic_blocks()) {
+                std::set<Value *> u;
+                std::set<Value *> d;
+                for (auto instr : bb->get_instructions()) {
+                    //TODO:求出每个bb的use和def
+                }
+                use[bb] = u;
+                def[bb] = d;
+            }
+            //书上算法
+            bool any_in_changed = true;
+            while(any_in_changed) {
+                any_in_changed = false;
+                for (auto B : func->get_basic_blocks()) {
+                    if (bb != ) {  //TODO: 除EXIT外的每个块B
+                        if () {  //TODO: S是B的后继
+                            //live_out[B] U live_in[S];
+                        }
+                        auto temp = live_out[B];  //TODO: live_out[B]-def[B]
+                        auto org_in = live_in[B];
+                        res.clear();
+                        //TODO: use[B] U temp;
+                        //TODO: auto res = org_in - live_in[B];
+                        if () { //如果in没变
+                            any_in_changed = true;
+                        }
+                    }
+                }
+            } 
             output_active_vars << print();
             output_active_vars << ",";
-        }
     }
     output_active_vars << "]";
     output_active_vars.close();
--- src/optimization/ConstPropagation.cpp
+++ src/optimization/ConstPropagation.cpp
@@ -31,6 +31,65 @@ ConstantInt *ConstFolder::compute(
         break;
     }
 }
+//TODO:修改float实现
+ConstantFP *ConstFolder::computefp(
+    Instruction::OpID op,
+    ConstantFP *value1,
+    ConstantFP *value2)
+{
+
+    int c_value1 = value1->get_value();
+    int c_value2 = value2->get_value();
+    switch (op)
+    {
+    case Instruction::fadd:
+        return ConstantFP::get(c_value1 + c_value2, module_);
+
+        break;
+    case Instruction::fsub:
+        return ConstantFP::get(c_value1 - c_value2, module_);
+        break;
+    case Instruction::fmul:
+        return ConstantFP::get(c_value1 * c_value2, module_);
+        break;
+    case Instruction::fdiv:
+        return ConstantFP::get((c_value1 / c_value2), module_);
+        break;
+    default:
+        return nullptr;
+        break;
+    }
+}
+ConstantInt *ConstFolder::compute(CmpInst::CmpOp op, ConstantInt *v1,
+                                ConstantInt *v2) {
+  int lhs = v1->get_value();
+  int rhs = v2->get_value();
+  int ret;
+  switch (op) {
+  case CmpInst::EQ:
+    ret = lhs == rhs;
+    break;
+  case CmpInst::NE:
+    ret = lhs != rhs;
+    break;
+  case CmpInst::GT:
+    ret = lhs > rhs;
+    break;
+  case CmpInst::GE:
+    ret = lhs >= rhs;
+    break;
+  case CmpInst::LE:
+    ret = lhs <= rhs;
+    break;
+  case CmpInst::LT:
+    ret = lhs < rhs;
+    break;
+  default:
+    return nullptr;
+    break;
+  }
+  return ConstantInt::get(ret, module_);
+}
 
 // 用来判断value是否为ConstantFP，如果不是则会返回nullptr
 ConstantFP *cast_constantfp(Value *value)
@@ -61,5 +120,27 @@ ConstantInt *cast_constantint(Value *value)
 
 void ConstPropagation::run()
 {
-    // 从这里开始吧！
+    auto func_list = m_->get_functions();
+    for (auto func : func_list) {
+        for (auto bb : func->get_basic_blocks()) {
+                for (auto instr : bb->get_instructions()) {
+                    if (instr->is_add() || instr->is_sub() || instr->is_mul() || instr->is_div() || instr->is_fadd() || instr->is_fsub() || instr->is_fmul() || instr->is_fdiv()) { 
+                        if (instr->is_add() || instr->is_sub() || instr->is_mul() || instr->is_div()) {
+                            auto l = cast_constantint(instr->get_operand(0));
+                            auto r = cast_constantint(instr->get_operand(1));
+                            if (l && r) {
+                                instr->replace_all_use_with(ConstFolder(m_).compute(instr->get_instr_type(), l, r));
+                            }
+                        }
+                        else {
+                            auto l = cast_constantfp(instr->get_operand(0));
+                            auto r = cast_constantfp(instr->get_operand(1));
+                            if (l && r) {
+                                instr->replace_all_use_with(ConstFolder(m_).computefp(instr->get_instr_type(), l, r));
+                            }
+                        }
+                    }
+            }
+        }
+    }
 }
